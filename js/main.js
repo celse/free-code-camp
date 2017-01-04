d489a38200b86ee0fef35e4867846f240496c8f3
Obou Brissy <Obou.Brissy@marionegri.it>
2017-01-04 18:28:39 +0100

    contact end

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+lat = 45.693161;
+lng = 9.671575;
 
 function main() {
 
@@ -34,53 +36,63 @@ function main() {
     });
 
 	// Hide nav on click
-  $(".navbar-nav li a").click(function (event) {
-    // check if window is small enough so dropdown is created
-    var toggle = $(".navbar-toggle").is(":visible");
-    if (toggle) {
-      $(".navbar-collapse").collapse('hide');
-    }
-  });
+    $(".navbar-nav li a").click(function (event) {
+        // check if window is small enough so dropdown is created
+        var toggle = $(".navbar-toggle").is(":visible");
+        if (toggle) {
+            $(".navbar-collapse").collapse('hide');
+        }
+    });
 	
-  	// Portfolio isotope filter
-    /*
-    $(window).load(function() {
-        var $container = $('.portfolio-items');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
 
-    });*/
+  	//  ============================
+    //  = Google MAP  =
+    //  ===========================
+
+    //var lat = 28.65850;
+    //var lng =   77.20341;
+
+    function initMap() {
+        var location = new google.maps.LatLng(lat, lng);
+        var mapCanvas = document.getElementById('contact_maps')
+        var mapOptions = {
+            center: location,
+            zoom: 17,
+            panControl: false,
+            scrollwheel: false,
+            mapTypeControl:false,
+            streetViewControl:false,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+
+        var markerImage = 'marker.png';
+        
+        var marker = new google.maps.Marker({
+            position: location,
+            map : map,
+            icom : markerImage   
+         });
+        /*marker.addDomListener('click', function(){
+            infowindow.open(map, marker);
+        });*/
+    }
+
+    google.maps.event.addDomListener(window, 'load', initMap);
+
+
+
 
     //  ============================
-        //  = Scroll event function =
-        //  ===========================
-        var goScrolling = function(elem) {
-            var docViewTop = $(window).scrollTop();
-            var docViewBottom = docViewTop + $(window).height();
-            var elemTop = elem.offset().top;
-            var elemBottom = elemTop + elem.height();
-            return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-        };
+    //  = Scroll event function =
+    //  ===========================
+    var goScrolling = function(elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+        var elemTop = elem.offset().top;
+        var elemBottom = elemTop + elem.height();
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    };
 
         
 	//  =======================
@@ -101,11 +113,12 @@ function main() {
         });
     });
     // Nivo Lightbox 
+    /*
     $('.portfolio-item a').nivoLightbox({
             effect: 'slideDown',  
             keyboardNav: true,                            
         });
-
+*/
 }());
 
 
